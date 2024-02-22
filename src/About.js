1407a118b6751aefa64437147ce14bdb8f46bef2
Devin <devincb93@gmail.com>
2024-02-22 15:23:39 -0800

    complete

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div>
         <h1>About</h1>
-        <h4>`PokePedia is your solution to keeping track of all your pokemon you've seen whether it's on Pokemon Go or any other Pokemon games. Simply add the name, and all the information you want to keep track of in the input fields on your Pokemon Home page. Once you've added this pokemon you'll be able to click on that pokemon and access all of it's information that you stored.`</h4>
+        <h4>`PokePedia is your solution to keeping track of all your pokemon you've seen whether it's on Pokemon Go or any other Pokemon games. Simply add the name, and all the information you want to keep track of in the input fields on your Pokemon Home page. Once you've added this pokemon you'll be able to click on that pokemon and access all of it's information that you've stored.`</h4>
         </div>
     )
 }
--- src/ReadMe.js
+++ src/ReadMe.js
@@ -8,6 +8,7 @@ function ReadMe() {
             <li>https://www.giantbomb.com/profile/wakka/lists/the-150-original-pokemon/59579/</li>
             <li>https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fnemotrex%2Fart%2Fbackground-prairie-pokemon-screencapture-861064355&psig=AOvVaw2nNj9Lao3kHCJq9JnFKFQ7&ust=1705915909662000&source=images&cd=vfe&ved=0CBMQjRxqFwoTCKDDspCW7oMDFQAAAAAdAAAAABAE</li>
             <li>https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ebay.com%2Fitm%2F115589115620&psig=AOvVaw29GBr9PMcARaFuURlMlVN2&ust=1705916789061000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCV5LOZ7oMDFQAAAAAdAAAAABAD</li>
+            <li>https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number</li>
         </div>
         </div>
     )
